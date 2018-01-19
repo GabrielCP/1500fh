25e241493f62e19a012f8b0333a3ccd61bf4087b
GabrielCP <35599518+GabrielCP@users.noreply.github.com>
2018-01-19 10:30:45 -0200

    Add files via upload

---
+++ WinApp/ApiFormControls/ItemsControl.Designer.cs
@@ -0,0 +1,211 @@
+﻿namespace WinApp
+{
+	partial class ItemsControl
+	{
+		/// <summary> 
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary> 
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Component Designer generated code
+
+		/// <summary> 
+		/// Required method for Designer support - do not modify 
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			System.Windows.Forms.Label label1;
+			System.Windows.Forms.ToolStrip toolStrip2;
+			System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(ItemsControl));
+			System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle1 = new System.Windows.Forms.DataGridViewCellStyle();
+			this.ProjectComboBox = new System.Windows.Forms.ToolStripComboBox();
+			this.ItemsGridView = new System.Windows.Forms.DataGridView();
+			this.toolStrip1 = new System.Windows.Forms.ToolStrip();
+			this.AddButton = new System.Windows.Forms.ToolStripButton();
+			this.DeleteButton = new System.Windows.Forms.ToolStripButton();
+			this.AddAttachmentButton = new System.Windows.Forms.ToolStripButton();
+			this.AxosoftHostLabel = new System.Windows.Forms.Label();
+			this.id = new System.Windows.Forms.DataGridViewTextBoxColumn();
+			this.name = new System.Windows.Forms.DataGridViewTextBoxColumn();
+			this.reportedDate = new System.Windows.Forms.DataGridViewTextBoxColumn();
+			label1 = new System.Windows.Forms.Label();
+			toolStrip2 = new System.Windows.Forms.ToolStrip();
+			toolStrip2.SuspendLayout();
+			((System.ComponentModel.ISupportInitialize)(this.ItemsGridView)).BeginInit();
+			this.toolStrip1.SuspendLayout();
+			this.SuspendLayout();
+			// 
+			// label1
+			// 
+			label1.AutoSize = true;
+			label1.Location = new System.Drawing.Point(18, 20);
+			label1.Name = "label1";
+			label1.Size = new System.Drawing.Size(113, 13);
+			label1.TabIndex = 8;
+			label1.Text = "Displaying Defects for:";
+			// 
+			// toolStrip2
+			// 
+			toolStrip2.AutoSize = false;
+			toolStrip2.Dock = System.Windows.Forms.DockStyle.None;
+			toolStrip2.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.ProjectComboBox});
+			toolStrip2.Location = new System.Drawing.Point(295, 41);
+			toolStrip2.Name = "toolStrip2";
+			toolStrip2.Size = new System.Drawing.Size(470, 25);
+			toolStrip2.TabIndex = 18;
+			toolStrip2.Text = "toolStrip2";
+			// 
+			// ProjectComboBox
+			// 
+			this.ProjectComboBox.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+			this.ProjectComboBox.Name = "ProjectComboBox";
+			this.ProjectComboBox.Size = new System.Drawing.Size(121, 25);
+			// 
+			// ItemsGridView
+			// 
+			this.ItemsGridView.AllowUserToAddRows = false;
+			this.ItemsGridView.AllowUserToDeleteRows = false;
+			this.ItemsGridView.AllowUserToResizeColumns = false;
+			this.ItemsGridView.AllowUserToResizeRows = false;
+			this.ItemsGridView.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+			this.ItemsGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+			this.ItemsGridView.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.id,
+            this.name,
+            this.reportedDate});
+			this.ItemsGridView.EditMode = System.Windows.Forms.DataGridViewEditMode.EditProgrammatically;
+			this.ItemsGridView.Location = new System.Drawing.Point(21, 69);
+			this.ItemsGridView.Name = "ItemsGridView";
+			this.ItemsGridView.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+			this.ItemsGridView.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+			this.ItemsGridView.Size = new System.Drawing.Size(744, 240);
+			this.ItemsGridView.TabIndex = 9;
+			// 
+			// toolStrip1
+			// 
+			this.toolStrip1.AutoSize = false;
+			this.toolStrip1.Dock = System.Windows.Forms.DockStyle.None;
+			this.toolStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.AddButton,
+            this.DeleteButton,
+            this.AddAttachmentButton});
+			this.toolStrip1.Location = new System.Drawing.Point(21, 41);
+			this.toolStrip1.Name = "toolStrip1";
+			this.toolStrip1.Size = new System.Drawing.Size(274, 25);
+			this.toolStrip1.TabIndex = 16;
+			this.toolStrip1.Text = "toolStrip1";
+			// 
+			// AddButton
+			// 
+			this.AddButton.Image = ((System.Drawing.Image)(resources.GetObject("AddButton.Image")));
+			this.AddButton.ImageTransparentColor = System.Drawing.Color.Magenta;
+			this.AddButton.Name = "AddButton";
+			this.AddButton.Size = new System.Drawing.Size(49, 22);
+			this.AddButton.Text = "Add";
+			this.AddButton.ToolTipText = "Add";
+			this.AddButton.Click += new System.EventHandler(this.AddButton_Click);
+			// 
+			// DeleteButton
+			// 
+			this.DeleteButton.Enabled = false;
+			this.DeleteButton.Image = ((System.Drawing.Image)(resources.GetObject("DeleteButton.Image")));
+			this.DeleteButton.ImageTransparentColor = System.Drawing.Color.Magenta;
+			this.DeleteButton.Name = "DeleteButton";
+			this.DeleteButton.Size = new System.Drawing.Size(60, 22);
+			this.DeleteButton.Text = "Delete";
+			this.DeleteButton.Click += new System.EventHandler(this.DeleteButton_Click);
+			// 
+			// AddAttachmentButton
+			// 
+			this.AddAttachmentButton.Enabled = false;
+			this.AddAttachmentButton.Image = ((System.Drawing.Image)(resources.GetObject("AddAttachmentButton.Image")));
+			this.AddAttachmentButton.ImageTransparentColor = System.Drawing.Color.Magenta;
+			this.AddAttachmentButton.Name = "AddAttachmentButton";
+			this.AddAttachmentButton.Size = new System.Drawing.Size(115, 22);
+			this.AddAttachmentButton.Text = "Add Attachment";
+			this.AddAttachmentButton.Click += new System.EventHandler(this.AddAttachment_Click);
+			// 
+			// AxosoftHostLabel
+			// 
+			this.AxosoftHostLabel.AutoSize = true;
+			this.AxosoftHostLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.AxosoftHostLabel.Location = new System.Drawing.Point(133, 20);
+			this.AxosoftHostLabel.Name = "AxosoftHostLabel";
+			this.AxosoftHostLabel.Size = new System.Drawing.Size(0, 13);
+			this.AxosoftHostLabel.TabIndex = 17;
+			// 
+			// id
+			// 
+			this.id.DataPropertyName = "Id";
+			this.id.HeaderText = "ID";
+			this.id.Name = "id";
+			this.id.Width = 50;
+			// 
+			// name
+			// 
+			this.name.DataPropertyName = "Name";
+			this.name.HeaderText = "Name";
+			this.name.Name = "name";
+			this.name.Width = 450;
+			// 
+			// reportedDate
+			// 
+			this.reportedDate.DataPropertyName = "ReportedDate";
+			dataGridViewCellStyle1.Format = "MM/dd/yyyy";
+			this.reportedDate.DefaultCellStyle = dataGridViewCellStyle1;
+			this.reportedDate.HeaderText = "Reported Date";
+			this.reportedDate.Name = "reportedDate";
+			this.reportedDate.Width = 200;
+			// 
+			// ItemsControl
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.Controls.Add(toolStrip2);
+			this.Controls.Add(this.AxosoftHostLabel);
+			this.Controls.Add(this.toolStrip1);
+			this.Controls.Add(this.ItemsGridView);
+			this.Controls.Add(label1);
+			this.Name = "ItemsControl";
+			this.Size = new System.Drawing.Size(787, 334);
+			toolStrip2.ResumeLayout(false);
+			toolStrip2.PerformLayout();
+			((System.ComponentModel.ISupportInitialize)(this.ItemsGridView)).EndInit();
+			this.toolStrip1.ResumeLayout(false);
+			this.toolStrip1.PerformLayout();
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.DataGridView ItemsGridView;
+		private System.Windows.Forms.ToolStrip toolStrip1;
+		private System.Windows.Forms.ToolStripButton AddButton;
+		private System.Windows.Forms.ToolStripButton DeleteButton;
+		private System.Windows.Forms.Label AxosoftHostLabel;
+		private System.Windows.Forms.ToolStripComboBox ProjectComboBox;
+		private System.Windows.Forms.ToolStripButton AddAttachmentButton;
+		private System.Windows.Forms.DataGridViewTextBoxColumn id;
+		private System.Windows.Forms.DataGridViewTextBoxColumn name;
+		private System.Windows.Forms.DataGridViewTextBoxColumn reportedDate;
+	}
+}
---
+++ WinApp/ApiFormControls/ItemsControl.cs
@@ -0,0 +1,274 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Net;
+using System.IO;
+using System.Security.Cryptography;
+using AxosoftAPI.NET.Models;
+using AxosoftAPI.NET;
+using AxosoftAPI.NET.Helpers;
+
+namespace WinApp
+{
+	public partial class ItemsControl : UserControl
+	{
+		Proxy AxosoftProxy;
+
+		BindingList<Project> Projects = new BindingList<Project>();
+		BindingList<Item> Items = new BindingList<Item>();
+
+		public ItemsControl()
+		{
+			InitializeComponent();
+
+			// configure project combo box in toolbar
+			ProjectComboBox.ComboBox.ValueMember = "Id";
+			ProjectComboBox.ComboBox.DisplayMember = "Name";
+			ProjectComboBox.ComboBox.DataSource = Projects;
+			ProjectComboBox.ComboBox.SelectedValueChanged += new EventHandler(ComboBox_SelectedValueChanged);
+
+			// configure the items grid
+			ItemsGridView.AutoGenerateColumns = false;
+			ItemsGridView.DataSource = Items;
+			ItemsGridView.CellEndEdit += new DataGridViewCellEventHandler(ItemsGridView_CellEndEdit);
+			ItemsGridView.SelectionChanged += new EventHandler(ItemsGridView_SelectionChanged);
+
+			System.Windows.Forms.Application.ApplicationExit += new EventHandler(Application_ApplicationExit);
+		}
+
+		void Application_ApplicationExit(object sender, EventArgs e)
+		{
+			if (AxosoftProxy != null && string.IsNullOrWhiteSpace(AxosoftProxy.AccessToken))
+			{
+				AxosoftProxy.Get<object>("oauth2/revoke");
+			}
+		}
+
+		public void SetAxosoftProxy(Proxy axosoftProxy)
+		{
+			AxosoftProxy = axosoftProxy;
+
+			// set Axosoft host label
+			AxosoftHostLabel.Text = new Uri(Program.Settings.Url).Host;
+
+			GetProjects();
+			if (Projects.Count == 0)
+			{
+				// there are no projects. ask the user if they want to create a new project.
+				var dialogResult = MessageBox.Show(
+					"Your Axosoft database has no projects. To use this example, you will need to create a project. Would you like to create a new project called \"API Example Project\" now?",
+					"Create an Axosoft scrum project",
+					MessageBoxButtons.YesNo,
+					MessageBoxIcon.Question);
+
+				if (dialogResult == DialogResult.Yes)
+				{
+					var project = new Project
+					{
+						Name = "API Example Project"
+					};
+
+					AxosoftProxy.Projects.Create(project);
+					GetProjects();
+				}
+				else
+				{
+					Application.Exit();
+					return;
+				}
+			}
+			GetItems();
+		}
+
+		void GetItems()
+		{
+			var result = AxosoftProxy.Defects.Get(new Dictionary<string, object> {
+				{ "project_id", ((Project)ProjectComboBox.SelectedItem).Id },
+				{ "sort_fields", "id desc" },
+				{ "page_size", 10 },
+				{ "columns", "id,name,reported_date" }
+			});
+
+			Items.Clear();
+
+			foreach (var item in result.Data)
+			{
+				Items.Add(item);
+			}
+		}
+
+		void GetProjects()
+		{
+			var result = AxosoftProxy.Projects.Get();
+
+			Projects.Clear();
+
+			foreach (var project in result.Data)
+			{
+				Projects.Add(project);
+			}
+		}
+
+		// called on grid selection change, enables / disables buttons as appropriate
+		void ItemsGridView_SelectionChanged(object sender, EventArgs e)
+		{
+			var count = ItemsGridView.SelectedRows.Count;
+			var newItemInSelection = ItemsGridView.SelectedRows.Cast<DataGridViewRow>().Any(row => row.Cells["id"].Value == null || (int)row.Cells["id"].Value == 0);
+			DeleteButton.Enabled = !newItemInSelection && (count > 0); // enable delete if any items are selected (and none of them is the new item)
+			AddAttachmentButton.Enabled = !newItemInSelection && count == 1; // only enable add attachment if one item is selected (and it's not the new item)
+		}
+
+		private void AddButton_Click(object sender, EventArgs e)
+		{
+			AddButton.Enabled = false;
+			Items.Insert(0, new Item());
+			DataGridViewCell newNameCell = ItemsGridView.Rows[0].Cells["name"];
+			ItemsGridView.CurrentCell = newNameCell;
+			ItemsGridView.BeginEdit(true);
+		}
+
+		private void ItemsGridView_CellEndEdit(object sender, DataGridViewCellEventArgs e)
+		{
+			var newItemName = (string)ItemsGridView.Rows[e.RowIndex].Cells[e.ColumnIndex].Value;
+
+			if (string.IsNullOrEmpty(newItemName))
+			{
+				// no name was entered - cancel add
+				Items.RemoveAt(0);
+			}
+			else
+			{
+				var item = new Item
+				{
+					Name = newItemName,
+					Project = new Project
+					{
+						Id = (int)ProjectComboBox.ComboBox.SelectedValue
+					}
+				};
+
+				AxosoftProxy.Defects.Create(item);
+
+				GetItems();
+			}
+			AddButton.Enabled = true;
+		}
+
+		private void DeleteButton_Click(object sender, EventArgs e)
+		{
+			var count = ItemsGridView.SelectedRows.Count;
+			if (count > 0)
+			{
+				var dialogResult = MessageBox.Show(
+					string.Format("This action is not reversible. Are you sure you want to delete {0} defect{1}?", count, count > 1 ? "s" : ""),
+					"Delete confirmation",
+					MessageBoxButtons.YesNo,
+					MessageBoxIcon.Warning);
+
+				if (dialogResult == DialogResult.Yes)
+				{
+					foreach (DataGridViewRow row in ItemsGridView.SelectedRows)
+						AxosoftProxy.Defects.Delete((int)row.Cells["id"].Value);
+					GetItems();
+				}
+			}
+		}
+
+		private void AddAttachment_Click(object sender, EventArgs e)
+		{
+			// have user select a file to attach
+			var openFileDialog = new OpenFileDialog();
+
+			if (openFileDialog.ShowDialog() == DialogResult.OK)
+			{
+				Stream stream;
+				if ((stream = openFileDialog.OpenFile()) != null)
+				{
+					using (stream)
+					{
+						// get id of selected item
+						var id = (int)ItemsGridView.CurrentRow.Cells["id"].Value;
+
+						var attachment = new Attachment
+						{
+							Data = stream,
+							FileName = Path.GetFileName(openFileDialog.FileName)
+						};
+
+						var result = AxosoftProxy.Defects.AddAttachment(id, attachment);
+
+						if (!result.IsSuccessful)
+						{
+							throw new AxosoftAPIException<ErrorResponse>(new ErrorResponse
+							{
+								Message = result.ErrorMessage,
+							});
+						}
+					}
+				}
+			}
+		}
+
+		private void ComboBox_SelectedValueChanged(object sender, EventArgs e)
+		{
+			GetItems();
+		}
+	}
+
+	// This is used to POST an attachment, due to a bug in Axosoft that expects the content to be encoded this way
+	class UTF8ByteEncoder : ICryptoTransform
+	{
+		public bool CanReuseTransform
+		{
+			get { return true; }
+		}
+
+		public bool CanTransformMultipleBlocks
+		{
+			get { return true; }
+		}
+
+		public int InputBlockSize
+		{
+			get { return 1; }
+		}
+
+		public int OutputBlockSize
+		{
+			get { return 2; }
+		}
+
+		public int TransformBlock(byte[] inputBuffer, int inputOffset, int inputCount, byte[] outputBuffer, int outputOffset)
+		{
+			var originalOutputOffset = outputOffset;
+			for (var inputIndex = 0; inputIndex < inputCount; inputIndex++)
+			{
+				var b = inputBuffer[inputOffset + inputIndex];
+				if ((b & 128) > 0)
+				{
+					outputBuffer[outputOffset++] = (byte)((b >> 6) | 0xc0);
+					outputBuffer[outputOffset++] = (byte)((b & 0x3f) | 0x80);
+				}
+				else
+					outputBuffer[outputOffset++] = b;
+			}
+			return outputOffset - originalOutputOffset;
+		}
+
+		public byte[] TransformFinalBlock(byte[] inputBuffer, int inputOffset, int inputCount)
+		{
+			var outputBuffer = new byte[inputBuffer.Length * 2];
+			var outputCount = TransformBlock(inputBuffer, inputOffset, inputCount, outputBuffer, 0);
+			return outputBuffer.Take(outputCount).ToArray();
+		}
+
+		public void Dispose()
+		{
+		}
+	}
+}
